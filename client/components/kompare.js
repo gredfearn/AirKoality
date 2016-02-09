33042caf16b37513da9f555a7f13d303445fd25b
Grant Redfearn <grantredfearn@gmail.com>
2016-02-08 19:41:49 -0600

    Added some labels

--- client/components/kompare.js
+++ client/components/kompare.js
@@ -30,16 +30,24 @@ console.log('descriptionOne', descriptionOne)
   m('h1', "Koalified Cities"),
   m('div', [
     m('h1', Koality.localOne),
+    m('h3', "Over Koality:"),
     m('p', descriptionOne),
+    m('h3', "Main Pollutant:"),
     m('p', mainpollutantOne),
+    m('h3', "Cause of Pollution:"),
     m('p', pollutionCauseOne),
+    m('h3', "Effects of Pollution:"),
     m('p', pollutionEffectOne)
     ]),
    m('div', [
     m('h1', Koality.localTwo),
+    m('h3', "Over Koality:"),
     m('p', descriptionTwo),
+    m('h3', "Main Pollutant:"),
     m('p', mainpollutantTwo),
+    m('h3', "Cause of Pollution:"),
     m('p', pollutionCauseTwo),
+    m('h3', "Effects of Pollution:"),
     m('p', pollutionEffectTwo)
     ]),
   m('button', {
